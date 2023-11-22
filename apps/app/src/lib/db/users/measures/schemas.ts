4d7c0ac6fad385b8ce16dd0dbc0b63dc63e965f3
KesvaL <47763141+xKesvaL@users.noreply.github.com>
2023-11-22 10:38:19 +0000

    feat: add user measures schemas & types

---
+++ apps/app/src/lib/db/users/measures/schemas.ts
@@ -0,0 +1,20 @@
+import * as z from "zod";
+
+export const UserMeasuresSchemas = z.object({
+  weight: z.number(),
+  waist: z.number(),
+  bodyFat: z.number(),
+  chest: z.number(),
+  bicepLeft: z.number(),
+  bicepRight: z.number(),
+  forearmLeft: z.number(),
+  forearmRight: z.number(),
+  abdomen: z.number(),
+  thighLeft: z.number(),
+  thighRight: z.number(),
+  calfLeft: z.number(),
+  calfRight: z.number(),
+  hips: z.number(),
+  neck: z.number(),
+  shoulder: z.number(),
+});
